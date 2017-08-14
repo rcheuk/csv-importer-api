bd8edd7f3d9bbcb1a828978387d2b7b8ca560ec4
Rachel Cheuk <rcheuk@scatteredhorizons.com>
2017-08-14 15:23:06 -0400

    updated with server saving files to server from upload api endpoint

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var Router = express.Router();
+var busboy = require('connect-busboy');
+
+var fs = require('fs');
+
 
 app.set('port', (process.env.PORT || 5000));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(busboy()); 
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,6 +23,22 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
+});
+
+app.post('/api/upload', function(req, res, next) {
+  var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/uploads/' + filename);
+        file.pipe(fstream);
+    });
+    req.busboy.on('finish', function() {
+      res.writeHead(200, { 'Connection': 'close' });
+      res.end("That's all folks!");
+    });
+
+    next();
 });
 
 app.listen(app.get('port'), function() {
